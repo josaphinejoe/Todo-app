6fe9b21aa8ac79a594c1569f3176d21f71990ef0
Tenzin <tenzin@Shreys-MacBook-Pro-2.local>
2023-02-22 23:57:05 -0500

    page level components

--- src/client/components/components.ts
+++ src/client/components/components.ts
@@ -1,21 +1,9 @@
-import { ShellViewModel } from "./shell/shell-view-model";
-import { NavBarViewModel } from "./nav-bar/nav-bar-view-model";
 import { FooterViewModel } from "./footer/footer-view-model";
-import { HeroSectionViewModel } from "./hero-section/hero-section-view-model";
-import { ValueSectionViewModel } from "./value-section/value-section-view-model";
-import { IntegrationsSectionViewModel } from "./integrations-section/integrations-section-view-model";
-import { FeaturesSectionViewModel } from "./features-section/features-section-view-model";
-import { ScaleSectionViewModel } from "./scale-section/scale-section-view-model";
-import { MarketingResourceSectionViewModel } from "./marketing-resource-section/marketing-resource-section-view-model";
+import { NavBarViewModel } from "./nav-bar/nav-bar-view-model";
+import { ShellViewModel } from "./shell/shell-view-model";
 
 export const components: Array<Function> = [
     ShellViewModel,
     NavBarViewModel,
-    FooterViewModel,
-    HeroSectionViewModel,
-    ValueSectionViewModel,
-    IntegrationsSectionViewModel,
-    FeaturesSectionViewModel,
-    ScaleSectionViewModel,
-    MarketingResourceSectionViewModel
+    FooterViewModel
 ];
